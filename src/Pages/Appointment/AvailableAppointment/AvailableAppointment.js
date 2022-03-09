53b3d5b48749c8b9338b51fb923489ed041b3790
QuaziSamiha <1703155@student.ruet.ac.bd>
2022-03-09 23:54:13 +0600

    get appointment part almost done

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,11 +1,61 @@
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
+import Booking from '../Booking/Booking';
 
-const AvailableAppointment = () => {
+const bookings = [
+    {
+        id: 1,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 2,
+        name: 'Cosmetic Dentistry',
+        time: '09.00 AM - 10.00 AM',
+        space: 8,
+    },
+    {
+        id: 3,
+        name: 'Teeth Cleaning',
+        time: '09.00 AM - 10.00 AM',
+        space: 9,
+    },
+    {
+        id: 4,
+        name: 'Cavity Protection',
+        time: '06.00 AM - 07.00 AM',
+        space: 10,
+    },
+    {
+        id: 5,
+        name: 'Pediatric Dental',
+        time: '06.00 AM - 07.00 AM',
+        space: 10,
+    },
+    {
+        id: 6,
+        name: 'Oral Surgery',
+        time: '07.00 AM - 08.00 AM',
+        space: 10,
+    }
+]
+const AvailableAppointment = ({ date }) => {
     return (
         <div>
-            <h2>
-                Available Appointment
-            </h2>
+            <Typography sx={{ color: 'info.main', mb: 2 }} variant='h5' gutterBottom component='div'>
+                Available Appointments on {date.toDateString()}
+            </Typography>
+            <Grid container spacing={2}>
+                {
+                    bookings.map((booking, index) => <Booking
+                        key={index}
+                        booking={booking}
+                        date={date}
+                    />
+                    )
+                }
+            </Grid>
         </div>
     );
 };
--- src/Pages/Shared/Calender/Calender.js
+++ src/Pages/Shared/Calender/Calender.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+import { SecurityUpdateGoodRounded } from '@mui/icons-material';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
+// import CalendarPicker from '@mui/lab/CalendarPicker';
+// import MonthPicker from '@mui/lab/MonthPicker';
+// import YearPicker from '@mui/lab/YearPicker';
+// import Grid from '@mui/material/Grid';
 
-const Calender = () => {
+// const minDate = new Date('2020-01-01T00:00:00.000');
+// const maxDate = new Date('2034-01-01T00:00:00.000');
+
+const Calender = ({ date, setDate }) => {
+
+    // const [date, setDate] = React.useState(new Date());
+    // const [date, setDate] = React.useState(new Date());
+
     return (
-        <div>
-            
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <StaticDatePicker
+                displayStaticWrapperAs='desktop'
+                value={date}
+                onChange={(newValue) => {
+                    setDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
+
+        // <LocalizationProvider dateAdapter={AdapterDateFns}>
+        //     <Grid container spacing={3}>
+        //         <Grid item xs={12} md={6}>
+        //             <CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
+        //         </Grid>
+        //         <Grid item xs={12} md={6}>
+        //             <MonthPicker
+        //                 date={date}
+        //                 minDate={minDate}
+        //                 maxDate={maxDate}
+        //                 onChange={(newDate) => setDate(newDate)}
+        //             />
+        //         </Grid>
+        //         <Grid item xs={12} md={6}>
+        //             <YearPicker
+        //                 date={date}
+        //                 isDateDisabled={() => false}
+        //                 minDate={minDate}
+        //                 maxDate={maxDate}
+        //                 onChange={(newDate) => setDate(newDate)}
+        //             />
+        //         </Grid>
+        //     </Grid>
+        // </LocalizationProvider>
     );
 };
 
